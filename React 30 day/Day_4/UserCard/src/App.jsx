8127003426b64c5e4823a1e7bb21a15579f27b57
KutsiGiira <haytamboualkhours@gmail.com>
2025-02-03 19:28:27 +0100

    making a card like myresume

--- React 30 day/Day_4/UserCard/src/App.jsx
+++ React 30 day/Day_4/UserCard/src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
-import Test from './Components/Card'
+import Skills, {Footer, Sut} from './Components/Card'
 function App() {
 
   return (
-      <Test />
-  )
+    <>
+      <Sut />
+      <Skills />
+      <Footer />
+    </>
+  );
 }
 
 export default App
--- React 30 day/Day_4/UserCard/src/Components/Card.jsx
+++ React 30 day/Day_4/UserCard/src/Components/Card.jsx
@@ -1,24 +1,39 @@
-import React from "react"
-// function Skills(){
-//     const Skill = [
-//         "Html",
-//         "Css",
-//         "Js",
-//         "React"
-//     ]
-// const listOfSkills =Skill.map((skill, index) => <li key={index}>{skill}</li>)
-//     return listOfSkills;
-// }
+    import React from "react"
+    import './des.css'
+    function Sut(){
+        return (
+            <>
+                <h2>Haytam</h2>
+                <span className="level">entry Level Developer, Morocco</span>
+            </>
+        )
+    }
+    function Skills(){
+        const Skill = [
+            "Html",
+            "Css",
+            "Js",
+            "React",
+            "java",
+            "spring boot"
+        ]
+    const listOfSkills = Skill.map((skill, index) => <li key={index} ><span className="list">{skill}</span></li>)
+        return (
+        <>
+            <div className="Skill-title">SKILLS</div>
+            <ul> 
+            {listOfSkills}
+            </ul>
 
-function Test(){
-    return <h1>Hello</h1>
-
-}
-function Test2(){
-    return <h3>Sub hello</h3>
-}
-
-// function Main(){
-//     return <div>{Test}{Test2}</div>
-// }
-export default Test;
+        </>
+        );
+    }
+    function Footer(){
+        return (
+            <footer>
+            joined on Jan 30,2025
+        </footer>
+        );
+    }
+export {Sut,Footer};
+export default Skills;
