2876360ad2607e5f0e7d7282e1608fcb5a0d851e
KutsiGiira <haytamboualkhours@gmail.com>
2025-02-03 18:50:03 +0100

    React components

---
+++ React 30 day/Day_4/ColorPalette/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="d1">#518cef</div>
+      <div className="d2">#3b10c4</div>
+      <div className="d3">#9aede6</div>
+      <div className="d4">#8ee763</div>
+      <div className="d5">#a30dd0</div>
+    </>
+  )
+}
+
+export default App
---
+++ React 30 day/Day_4/UserCard/src/Components/Card.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+// function Skills(){
+//     const Skill = [
+//         "Html",
+//         "Css",
+//         "Js",
+//         "React"
+//     ]
+// const listOfSkills =Skill.map((skill, index) => <li key={index}>{skill}</li>)
+//     return listOfSkills;
+// }
+
+function Test(){
+    return <h1>Hello</h1>
+
+}
+function Test2(){
+    return <h3>Sub hello</h3>
+}
+
+// function Main(){
+//     return <div>{Test}{Test2}</div>
+// }
+export default Test;
