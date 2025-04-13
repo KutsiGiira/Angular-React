dc901b7cbdd313e01587e34434bf4d5f93c4acb2
KutsiGiira <haytamboualkhours@gmail.com>
2025-04-14 00:30:47 +0100

    testing for responsive desktop/mobile

--- Ai/project/src/screens/Aicademy/sections/imageheader/imageheader.tsx
+++ Ai/project/src/screens/Aicademy/sections/imageheader/imageheader.tsx
@@ -3,17 +3,21 @@ import header_img from "../assets/header_img.png";
 
 export const ImageHeader = (): JSX.Element => {
   return (
-    <div className="relative w-full flex justify-center items-center py-16 overflow-visible bg-gradient-to-b from-white via-white to-white ">
-      {/* Blur Circles */}
-      <div className="absolute left-[-150px] top-[5px] md:w-[315px] md:h-[800px] w-[170px] h-[600px] bg-[#46B8FF] rounded-full blur-[150px] opacity-50 z-0" />
-      <div className="absolute right-[-150px] top-[5px] md:w-[315px] md:h-[800px] w-[170px] h-[600px] bg-[#8958FE] rounded-full blur-[150px] opacity-50 z-0" />
+    <div className="relative w-full flex justify-center items-center py-16 bg-white overflow-hidden">
+      {/* Left Blur Circle - looks good on mobile & desktop */}
+      <div className="pointer-events-none absolute top-25 left-0 translate-x-[-30%] w-[60px] h-[180px] md:w-[250px] md:h-[400px] bg-[#46B8FF]  rounded-full blur-[40px] md:blur-[100px] opacity-50 md:opacity-30 z-0" />
+
+      {/* Right Blur Circle */}
+      <div className="pointer-events-none absolute top-25 right-0 translate-x-[30%] w-[60px] h-[180px] md:w-[250px] md:h-[400px] bg-[#8958FF] rounded-full  blur-[40px] md:blur-[100px] opacity-50 md:opacity-30 z-0" />
       
       {/* Main Image */}
       <img
         className="relative z-10 w-full max-w-6xl px-4"
         src={header_img}
         alt="Header"
+        loading="lazy"
       />
     </div>
   );
 };
+
